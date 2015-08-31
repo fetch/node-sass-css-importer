fa5128c453105667a1df13bdc749645d16f9e5f8
Koen Punt <koen@koenpunt.nl>
2015-08-31 22:45:38 +0200

    add tests

---
+++ test/test.js
@@ -0,0 +1,28 @@
+var fs = require('fs')
+  , path = require('path')
+  , assert = require('assert');
+
+var node = require('node-sass')
+  , cssImporter = require('../');
+
+node.render({
+  file: path.join(__dirname, 'source.scss'),
+  importer: cssImporter({import_paths: [path.join(__dirname, 'precedence')]})
+}, function(err, actual) {
+  assert.equal(err, null);
+  fs.readFile(path.join(__dirname, 'expected.css'), function(err, expected) {
+    assert.equal(err, null);
+    assert.equal(actual.css.toString(), expected.toString());
+  });
+});
+
+node.render({
+  data: 'html{font-size: 10px}@import "CSS:body";',
+  importer: cssImporter({import_paths: [__dirname]})
+}, function(err, actual) {
+  assert.equal(err, null);
+  fs.readFile(path.join(__dirname, 'expected.css'), function(err, expected) {
+    assert.equal(err, null);
+    assert.equal(actual.css.toString(), expected.toString());
+  });
+});
