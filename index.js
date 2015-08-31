aa4b052a3765874cfa2b4ed157e8ffe01f907525
Koen Punt <koen@koenpunt.nl>
2015-08-31 22:44:48 +0200

    take path of previous file into account
    also prevent hangs on when there are no import paths

--- index.js
+++ index.js
@@ -2,20 +2,30 @@ var fs = require('fs')
   , path = require('path');
 
 module.exports = function(options) {
-  options = options || {};
+  options = options || {import_paths: []};
 
-  var import_paths = options.import_paths || []
-    , import_paths_len = import_paths.length;
+  var import_paths
+    , import_paths_len;
   
   return function(url, prev, done) {
     if (url.slice(0, 4) !== 'CSS:') {
       return done();
     }
 
-    var css_path = url.slice(4) + '.css'
-      , css_filepath, i, import_path;
+    import_paths = options.import_paths.slice();
+    if (fs.existsSync(prev)) {
+      import_paths.unshift(path.dirname(prev));
+    }
+    import_paths_len = import_paths.length;
 
-    for (i = 0; i < import_paths_len; i++) {
+    if (import_paths_len === 0) {
+      return done();
+    }
+
+    var css_path = url.slice(4) + '.css'
+      , css_filepath, i = 0, import_path;
+
+    for (; i < import_paths_len; ++i) {
       import_path = import_paths[i];
       css_filepath = path.join(import_path, css_path);
       if (fs.existsSync(css_filepath)) {
@@ -23,6 +33,7 @@ module.exports = function(options) {
           if (err) return console.error(err);
           done({contents: data.toString()});
         });
+        break;
       }
     }
   };
